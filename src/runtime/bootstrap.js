c7bc94c96857e60184d72156418a64a94a4d16ff
maxandbe1 <maxandbe1@proton.me>
2026-05-29 19:20:28 -0400

    Create bootstrap.js

---
+++ src/runtime/bootstrap.js
@@ -0,0 +1,8 @@
+if (!window.Portal) window.Portal = {};
+if (!window.Portal.modules) window.Portal.modules = {};
+if (!window.Portal.registry) window.Portal.registry = {};
+
+window.Portal.registry.identity = {
+  key: "identity",
+  label: "Identity"
+};
