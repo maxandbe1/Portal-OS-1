0c7311515416bd710e98082612c7183cfc7b8cc5
maxandbe1 <maxandbe1@proton.me>
2026-05-30 18:36:37 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./styles.css";
+import { bootstrapPortal } from "./runtime/bootstrap.js";
+
+bootstrapPortal();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
