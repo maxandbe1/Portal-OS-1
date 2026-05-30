a8eef6b2c8d9d8567e2382f90d13108244c5a167
maxandbe1 <maxandbe1@proton.me>
2026-05-29 22:44:17 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
-5import { defineConfig } from "vite";
+
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  root: ".",
+  publicDir: "public",
+  build: {
+    outDir: "dist",
+    emptyOutDir: true
+  },
   server: {
-    hmr: { overlay: false }
+    port: 5173
   }
 });
